ed163bae5d4cce0d95ef2b53b71f59149253df73
Ilya Radu <raduilya@gmail.com>
2017-06-25 18:52:39 +0300

    Webrtc Signaling &  Ice candidate

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-let express = require('express');
-let app = express();
-console.log('server started on port 3000');
+var express = require('express.io');
+var app = express();
+app.http().io();
+var PORT = 3000;
+console.log('server started on port:' , PORT);
 
 app.use(express.static(__dirname + '/public'));
 
@@ -8,4 +10,26 @@ app.get('/',(req, res) => {
     res.render('index.ejs');
 });
 
-app.listen(3000)
+app.io.route('ready',(req) => {
+    req.io.join(req.data.chat_room);
+    req.io.join(req.data.signal_room);
+    app.io.room(req.data).broadcast('announce',{
+        message: 'New client in the ' + req.data + 'room.'
+    })
+})
+
+app.io.route('send',(req) => {
+    app.io.room(req.data.room).broadcast('message', {
+        message: req.data.message,
+        author: req.data.author
+    });
+})
+
+app.io.route('signal',(req) => {
+    // Note the use of req for broadcasting so only the sender doest`t their own message
+    req.io.room(req.data.room).broadcast('signaling_message', {
+        type: req.data.type,
+        message: req.data.message
+    });
+})
+app.listen(PORT);
